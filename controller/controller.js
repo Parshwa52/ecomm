084832621173f1e5c963d0baa9f91203a5f7ef44
Parshwa52 <parshwashahsbmp52@gmail.com>
2023-12-06 13:57:27 +0530

    added user and admin controllers linked to service

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,30 @@
 import express from "express";
 
-import { addToCart, getCart } from "../service/service.js";
+import {
+  addToCart,
+  checkout,
+  generatePromoCode,
+  getCart,
+  countOfItemsPurchased,
+  getTotalPurchaseAmount,
+  getTotalDiscountAmount,
+  listDiscountCodes,
+  listOrders,
+} from "../service/service.js";
 
 const router = express.Router();
 
+//User APIs
 router.post("/addToCart", addToCart);
+router.post("/checkout", checkout);
+
+//Admin APIs
 router.get("/cart", getCart);
+router.post("/generatePromoCode", generatePromoCode);
+router.get("/itemsPurchased", countOfItemsPurchased);
+router.get("/totalPurchaseAmount", getTotalPurchaseAmount);
+router.get("/totalDiscountAmount", getTotalDiscountAmount);
+router.get("/discountCodes", listDiscountCodes);
+router.get("/orders", listOrders);
 
 export default router;
